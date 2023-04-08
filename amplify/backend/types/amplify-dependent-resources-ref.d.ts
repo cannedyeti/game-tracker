043c28854092def32e5c78cbde6a10fe7dd1f793
cannedyeti <cpotebnya@gmail.com>
2023-04-07 18:08:29 -0700

    add cognito

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "gametracker8f9c3bc7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-function App() {
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+function App({ signOut, user }) {
   return (
     <div className="App">
-      This is a change.
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+      <h2>Get this shit done boyos</h2>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
